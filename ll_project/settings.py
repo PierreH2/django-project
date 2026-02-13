e927733efce627a019351b1fa95c0f3d6bac25fe
piron <pierre.huang.pro@gmail.com>
2026-02-14 00:31:28 +0100

    first django project + boostrap5 styling

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'learning_logs',
+    'accounts',
+    'django_bootstrap5',
+    #default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# My settings.
+LOGIN_REDIRECT_URL = 'learning_logs:index'
+LOGOUT_REDIRECT_URL = 'learning_logs:index'
+LOGIN_URL = 'accounts:login'
